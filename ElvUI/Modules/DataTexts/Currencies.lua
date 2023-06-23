9e05634191ce537226f0be094a8c922d96ec0531
Crumdidlyumshis <stschleyer89@gmail.com>
2023-06-23 18:19:20 -0500

    CustomCurrency doesn't work in wrath (only classic wrath)

--- ElvUI/Modules/DataTexts/Currencies.lua
+++ ElvUI/Modules/DataTexts/Currencies.lua
@@ -1,30 +1,31 @@
 local E, L, V, P, G = unpack(ElvUI)
-local DT = E:GetModule('DataTexts')
+local DT = E:GetModule("DataTexts")
 
 local _G = _G
 local format, tonumber, wipe = format, tonumber, wipe
 local pairs, ipairs, unpack, tostring = pairs, ipairs, unpack, tostring
-local BreakUpLargeNumbers = BreakUpLargeNumbers
 local GetMoney = GetMoney
 
-local BONUS_ROLL_REWARD_MONEY = BONUS_ROLL_REWARD_MONEY
+local CURRENCY = CURRENCY
+local BACKPACK_TOOLTIP = BACKPACK_TOOLTIP
+local GUILDCONTROL_OPTION16 = GUILDCONTROL_OPTION16
 
-local iconString, db = '|T%s:16:16:0:0:64:64:4:60:4:60|t'
-DT.CurrencyList = { GOLD = BONUS_ROLL_REWARD_MONEY, BACKPACK = 'Backpack' }
+local iconString, db = "|T%s:16:16:0:0:64:64:4:60:4:60|t"
+DT.CurrencyList = { GOLD = GUILDCONTROL_OPTION16, BACKPACK = "Backpack" }
 
 local function OnClick()
-	_G.ToggleCharacter('TokenFrame')
+	ToggleCharacter("TokenFrame")
 end
 
 local function AddInfo(id)
 	local info, name, icon = DT:CurrencyInfo(id)
 	if name then
-		local textRight = '%s'
+		local textRight = "%s"
 		if db.maxCurrency and info.maxQuantity and info.maxQuantity > 0 then
-			textRight = '%s / '..BreakUpLargeNumbers(info.maxQuantity)
+			textRight = "%s / "..E:BreakUpLargeNumbers(info.maxQuantity)
 		end
 
-		DT.tooltip:AddDoubleLine(format('%s %s', icon, name), format(textRight, BreakUpLargeNumbers(info.quantity)), 1, 1, 1, 1, 1, 1)
+		DT.tooltip:AddDoubleLine(format("%s %s", icon, name), format(textRight, E:BreakUpLargeNumbers(info.quantity)), 1, 1, 1, 1, 1, 1)
 	end
 end
 
@@ -33,7 +34,7 @@ local function AddHeader(id, addLine)
 	if (not db.headers) or shownHeaders[id] then return end
 
 	if addLine then
-		DT.tooltip:AddLine(' ')
+		DT.tooltip:AddLine(" ")
 	end
 
 	DT.tooltip:AddLine(db.tooltipData[id][1])
@@ -42,20 +43,20 @@ end
 
 local goldText
 local function OnEvent(self)
-	goldText = E:FormatMoney(GetMoney(), db.goldFormat or 'BLIZZARD', not db.goldCoins)
+	goldText = E:FormatMoney(GetMoney(), db.goldFormat or "BLIZZARD", not db.goldCoins)
 
 	local displayed = db.displayedCurrency
-	if displayed == 'BACKPACK' then
+	if displayed == "BACKPACK" then
 		local displayString
 		for i = 1, 3 do
 			local info = DT:BackpackCurrencyInfo(i)
 			if info and info.quantity then
-				displayString = (i > 1 and displayString..' ' or '')..format('%s %s', format(iconString, info.iconFileID), E:ShortValue(info.quantity))
+				displayString = (i > 1 and displayString.." " or "")..format("%s %s", format(iconString, info.iconFileID), E:ShortValue(info.quantity))
 			end
 		end
 
 		self.text:SetText(displayString or goldText)
-	elseif displayed == 'GOLD' then
+	elseif displayed == "GOLD" then
 		self.text:SetText(goldText)
 	else
 		local id = tonumber(displayed)
@@ -65,12 +66,12 @@ local function OnEvent(self)
 		if not name then return end
 
 		local style = db.displayStyle
-		if style == 'ICON' then
-			self.text:SetFormattedText('%s %s', icon, E:ShortValue(info.quantity))
-		elseif style == 'ICON_TEXT' then
-			self.text:SetFormattedText('%s %s %s', icon, name, E:ShortValue(info.quantity))
+		if style == "ICON" then
+			self.text:SetFormattedText("%s %s", icon, E:ShortValue(info.quantity))
+		elseif style == "ICON_TEXT" then
+			self.text:SetFormattedText("%s %s %s", icon, name, E:ShortValue(info.quantity))
 		else --ICON_TEXT_ABBR
-			self.text:SetFormattedText('%s %s %s', icon, E:AbbreviateString(name), E:ShortValue(info.quantity))
+			self.text:SetFormattedText("%s %s %s", icon, E:AbbreviateString(name), E:ShortValue(info.quantity))
 		end
 	end
 end
@@ -90,21 +91,10 @@ local function OnEnter()
 	end
 
 	if addLine then
-		DT.tooltip:AddLine(' ')
+		DT.tooltip:AddLine(" ")
 	end
 
-	for id, info in pairs(E.global.datatexts.customCurrencies) do
-		if info and not DT.CurrencyList[tostring(id)] and info.currencyTooltip then
-			AddInfo(id)
-			addLine2 = true
-		end
-	end
-
-	if addLine2 then
-		DT.tooltip:AddLine(' ')
-	end
-
-	DT.tooltip:AddDoubleLine(L["Gold"]..':', goldText, nil, nil, nil, 1, 1, 1)
+	DT.tooltip:AddDoubleLine(L["Gold"]..":", goldText, nil, nil, nil, 1, 1, 1)
 	DT.tooltip:Show()
 end
 
@@ -114,4 +104,4 @@ local function ApplySettings(self)
 	end
 end
 
-DT:RegisterDatatext('Currencies', nil, { 'PLAYER_MONEY', 'SEND_MAIL_MONEY_CHANGED', 'SEND_MAIL_COD_CHANGED', 'PLAYER_TRADE_MONEY', 'TRADE_MONEY_CHANGED', 'CHAT_MSG_CURRENCY', 'CURRENCY_DISPLAY_UPDATE', 'PERKS_PROGRAM_CURRENCY_REFRESH' }, OnEvent, nil, OnClick, OnEnter, nil, _G.CURRENCY, nil, ApplySettings)
+DT:RegisterDatatext("Currencies", nil, { "PLAYER_MONEY", "SEND_MAIL_MONEY_CHANGED", "SEND_MAIL_COD_CHANGED", "PLAYER_TRADE_MONEY", "TRADE_MONEY_CHANGED", "CHAT_MSG_CURRENCY", "CURRENCY_DISPLAY_UPDATE" }, OnEvent, nil, OnClick, OnEnter, nil, CURRENCY, nil, ApplySettings)
