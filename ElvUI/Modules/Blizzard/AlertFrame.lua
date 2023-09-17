5315dad1700aaadd391f40d19bbbd99f247a75e7
Crumdidlyumshis <stschleyer89@gmail.com>
2023-09-17 15:06:01 -0500

    Forgot this in LootRoll

--- ElvUI/Modules/Blizzard/AlertFrame.lua
+++ ElvUI/Modules/Blizzard/AlertFrame.lua
@@ -8,90 +8,25 @@ local pairs = pairs
 --WoW API / Variables
 local NUM_GROUP_LOOT_FRAMES = NUM_GROUP_LOOT_FRAMES
 
-local POSITION, ANCHOR_POINT, YOFFSET = "TOP", "BOTTOM", -10
+local POSITION, ANCHOR_POINT, Y_OFFSET, BASE_YOFFSET = 'TOP', 'BOTTOM', -5, 0 -- should match in PostAlertMove
 
 function E:PostAlertMove()
+	local AlertFrame = _G.AlertFrame
+	local AlertFrameMover = _G.AlertFrameMover
+
 	local _, y = AlertFrameMover:GetCenter()
-	local screenHeight = E.UIParent:GetTop()
-	if y > (screenHeight / 2) then
-		POSITION = "TOP"
-		ANCHOR_POINT = "BOTTOM"
-		YOFFSET = -10
-		AlertFrameMover:SetText(AlertFrameMover.textString.." [Grow Down]")
-	else
-		POSITION = "BOTTOM"
-		ANCHOR_POINT = "TOP"
-		YOFFSET = 10
-		AlertFrameMover:SetText(AlertFrameMover.textString.." [Grow Up]")
+	local growUp = y < (E.UIParent:GetTop() * 0.5)
+
+	if growUp then
+		POSITION, ANCHOR_POINT, Y_OFFSET, BASE_YOFFSET = 'BOTTOM', 'TOP', 5, 0
+	else -- should match above in the cache
+		POSITION, ANCHOR_POINT, Y_OFFSET, BASE_YOFFSET = 'TOP', 'BOTTOM', -5, 0
 	end
 
-	if E.private.general.lootRoll then
-		local lastframe, lastShownFrame
-		for i, frame in pairs(Misc.RollBars) do
-			frame:ClearAllPoints()
-			if i ~= 1 then
-				if POSITION == "TOP" then
-					frame:Point("TOP", lastframe, "BOTTOM", 0, -4)
-				else
-					frame:Point("BOTTOM", lastframe, "TOP", 0, 4)
-				end
-			else
-				if POSITION == "TOP" then
-					frame:Point("TOP", AlertFrameHolder, "BOTTOM", 0, -4)
-				else
-					frame:Point("BOTTOM", AlertFrameHolder, "TOP", 0, 4)
-				end
-			end
-			lastframe = frame
+	AlertFrameMover:SetFormattedText('%s %s', AlertFrameMover.textString, growUp and '(Grow Up)' or '(Grow Down)')
 
-			if frame:IsShown() then
-				lastShownFrame = frame
-			end
-		end
-
-		AlertFrame:ClearAllPoints()
-		if lastShownFrame then
-			AlertFrame:SetAllPoints(lastShownFrame)
-		else
-			AlertFrame:SetAllPoints(AlertFrameHolder)
-		end
-	elseif E.private.skins.blizzard.enable and E.private.skins.blizzard.lootRoll then
-		local lastframe, lastShownFrame
-		for i = 1, NUM_GROUP_LOOT_FRAMES do
-			local frame = _G["GroupLootFrame"..i]
-			if frame then
-				frame:ClearAllPoints()
-				if i ~= 1 then
-					if POSITION == "TOP" then
-						frame:Point("TOP", lastframe, "BOTTOM", 0, -4)
-					else
-						frame:Point("BOTTOM", lastframe, "TOP", 0, 4)
-					end
-				else
-					if POSITION == "TOP" then
-						frame:Point("TOP", AlertFrameHolder, "BOTTOM", 0, -4)
-					else
-						frame:Point("BOTTOM", AlertFrameHolder, "TOP", 0, 4)
-					end
-				end
-				lastframe = frame
-
-				if frame:IsShown() then
-					lastShownFrame = frame
-				end
-			end
-		end
-
-		AlertFrame:ClearAllPoints()
-		if lastShownFrame then
-			AlertFrame:SetAllPoints(lastShownFrame)
-		else
-			AlertFrame:SetAllPoints(AlertFrameHolder)
-		end
-	else
-		AlertFrame:ClearAllPoints()
-		AlertFrame:SetAllPoints(AlertFrameHolder)
-	end
+	AlertFrame:ClearAllPoints()
+	AlertFrame:SetAllPoints((E.private.general.lootRoll and Misc:UpdateLootRollAnchors(POSITION)) or _G.AlertFrameHolder)
 end
 
 function B:AchievementAlertFrame_FixAnchors()
