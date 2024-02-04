8b2167aff20d107b3d357ce0f48bed267b75b53f
Crumdidlyumshis <stschleyer89@gmail.com>
2024-02-04 15:52:51 -0600

    Revert "more to tags"
    This reverts commit edadb28200bea1a5127e05a0a3ea451a3fc90b3c.

--- ElvUI/Libraries/oUF/elements/tags.lua
+++ ElvUI/Libraries/oUF/elements/tags.lua
@@ -501,7 +501,7 @@ local tagEvents = {
 	['shortclassification']	= 'UNIT_CLASSIFICATION_CHANGED',
 	['smartlevel']			= 'UNIT_LEVEL PLAYER_LEVEL_UP UNIT_CLASSIFICATION_CHANGED',
 	['soulshards']          = 'UNIT_INVENTORY_CHANGED',
-	['status']				= 'UNIT_HEALTH PLAYER_UPDATE_RESTING UNIT_CONNECTION',
+	['status']				= 'UNIT_HEALTH PLAYER_UPDATE_RESTING',
 	['threat']				= 'UNIT_THREAT_SITUATION_UPDATE',
 	['threatcolor']			= 'UNIT_THREAT_SITUATION_UPDATE',
 }
