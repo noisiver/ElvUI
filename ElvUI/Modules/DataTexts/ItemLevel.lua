f77d184101f106eaaa99e381fc5000de9dcb9335
Revision <tkn963@gmail.com>
2024-03-26 03:39:47 +0100

    Item Level
    Use the already existing function to get the item level.

--- ElvUI/Modules/DataTexts/ItemLevel.lua
+++ ElvUI/Modules/DataTexts/ItemLevel.lua
@@ -1,37 +1,12 @@
 local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local DT = E:GetModule("DataTexts")
+local TT = E:GetModule("Tooltip")
 
 local join = string.join
-
 local displayNumberString = ""
 
-local function GetAverageItemLevel()
-    local items = 16
-    local ilvl = 0
-
-    for slotID = 1, 18 do
-        if slotID ~= INVSLOT_BODY then
-            local itemLink = GetInventoryItemLink("player", slotID)
-
-            if itemLink then
-                local _, _, quality, itemLevel, _, _, _, _, itemEquipLoc = GetItemInfo(itemLink)
-
-                if itemLevel then
-                    ilvl = ilvl + itemLevel
-
-                    if slotID == INVSLOT_MAINHAND and (itemEquipLoc ~= "INVTYPE_2HWEAPON" or titanGrip) then
-                        items = 17
-                    end
-                end
-            end
-        end
-    end
-
-    return ilvl / items
-end
-
 local function OnEvent(self)
-    self.text:SetFormattedText(displayNumberString, L["Item Level"], GetAverageItemLevel())
+    self.text:SetFormattedText(displayNumberString, L["Item Level"], TT:GetItemLvL("player"))
 end
 
 local function ValueColorUpdate(hex)
